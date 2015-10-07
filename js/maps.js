03a29aee925aaaaf0c3d1f9184a616651a5e7ea1
Amber <aford2008@gmail.com>
2015-10-07 16:46:36 -0400

    added 1890s info to the map

--- js/maps.js
+++ js/maps.js
@@ -15,10 +15,22 @@ var locations = [
      ['1929- The Ambassador Bridge is completed and opens on November 11th.', 42.316689, -83.077232, 13],
      ['1927- On May 26th, Henry Ford ends production of the Model T.'  , 42.316689, -83.077232, 14],
      ['1927- The Purple Gang was the most notorious organized crime gang in Detroit in the 1920s and 1930s. The gang was made up of Jewish immigrants from the lower east side of Detroit. They were involved in murders, smuggling, extortion, kidnapping, gambling and more.', 42.391415, -83.070282, 15],
-     ['1928- The 47-story Penobscot building was built. At the time, it was the tallest structure in Michigan and the 8th tallest building in the world.', 42.330547, -83.047419, 16]
+     ['1928- The 47-story Penobscot building was built. At the time, it was the tallest structure in Michigan and the 8th tallest building in the world.', 42.330547, -83.047419, 16],
+
+     ['1896- On June 4th, Henry Ford drives his first automobile through the streets of Detroit.', 42.338041, -83.051588, 17],
+     ["1896- Central High School is built on the corner of Cass and Warren. Today, it is Wayne State University's Old Main building.", 42.355046, -83.067359, 18],
+     ["1896- The last horse-drawn carriage is replaced by electric trolleys.", 42.347054, -83.055765, 19],
+     ["1892- Four Detroit streets (Jefferson, Lafayette, Cass and Second) are paved with asphalt. The rest are cobblestone, cedar block or unpaved.", 42.331389, -83.038246, 20],
+     ["1893- Detroiter Marie Owen beomes the first policewoman in the United States.", 42.346292, -83.078598, 21],
+     ["1899- William Metzger and Seneca Lewis organize Detroit's first auto show. They showed 4 cars; 2 electric and 2 steam powered.", 42.338560, -83.028546, 22], 
+     ["1895- The Detroit Public Schools establish Kindergartens.", 42.333921, -83.054699, 23],
+     ["1895- Detroit begins to supply power for streetlights and public buildings.", 42.335646, -83.041582, 24],
+     ["1890- Detroit's population is 205,876; the 15th largest city in America.", 42.336657, -83.046571, 25]
+     
 
 
 ];
+
 var map = new google.maps.Map(document.getElementById('map'), {
      zoom: 14,
      center: new google.maps.LatLng(42.334498, -83.049645),
