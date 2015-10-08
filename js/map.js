b35b689bc2a3484c55c834e7009b43c7c06dab71
Matthew <Mattxsc@gmail.com>
2015-10-08 12:03:07 -0400

    changed default map to current map and cleaned up code

--- js/map.js
+++ js/map.js
@@ -159,6 +159,7 @@ app.controller('mapCtrl', function($scope) {
       // Add the element to the "overlayLayer" pane.
       var panes = this.getPanes();
       panes.overlayLayer.appendChild(div);
+      overlay.hide();
     };
     // [END region_attachment]
 
@@ -207,66 +208,6 @@ app.controller('mapCtrl', function($scope) {
         this.div_.style.visibility = 'visible';
       }
     };
-
-
-    /**
-     * The CenterControl adds a control to the map that recenters the map on
-     * Chicago.
-     * @constructor
-     * @param {!Element} controlDiv
-     * @param {!google.maps.Map} map
-     * @param {?google.maps.LatLng} center
-     */
-    function CenterControl(controlDiv, map, center) {
-      // We set up a variable for this since we're adding event listeners later.
-      var control = this;
-
-      // Set the center property upon construction
-      control.center_ = center;
-      controlDiv.style.clear = 'both';
-
-      // Set CSS for the control border
-      var goCenterUI = document.createElement('div');
-      goCenterUI.id = 'goCenterUI';
-      goCenterUI.title = 'Click to recenter the map';
-      controlDiv.appendChild(goCenterUI);
-
-      // Set CSS for the control interior
-      var goCenterText = document.createElement('div');
-      goCenterText.id = 'goCenterText';
-      goCenterText.innerHTML = '1890s';
-      goCenterUI.appendChild(goCenterText);
-
-      // Set CSS for the setCenter control border
-      var setCenterUI = document.createElement('div');
-      setCenterUI.id = 'setCenterUI';
-      setCenterUI.title = 'Click to change the center of the map';
-      controlDiv.appendChild(setCenterUI);
-
-      // Set CSS for the control interior
-      var setCenterText = document.createElement('div');
-      setCenterText.id = 'setCenterText';
-      setCenterText.innerHTML = 'Today';
-      setCenterUI.appendChild(setCenterText);
-
-      // Set up the click event listener for 'Center Map': Set the center of the map
-      // to the current center of the control.
-      goCenterUI.addEventListener('click', function() {
-        overlay.show();
-      });
-
-      // Set up the click event listener for 'Set Center': Set the center of the
-      // control to the current center of the map.
-      setCenterUI.addEventListener('click', function() {
-        overlay.hide();
-      });
-    }
-
-    /**
-     * Define a property to hold the center state.
-     * @private
-     */
-    CenterControl.prototype.center_ = null;
 
 
   google.maps.event.addDomListener(window, 'load', initMap);
