3cd7f6e8acffb04cd536780b47ec0f0183a61702
Matthew <Mattxsc@gmail.com>
2015-10-13 15:25:43 -0400

    added random fact generator

--- js/1890Ctrl.js
+++ js/1890Ctrl.js
@@ -1,5 +1,19 @@
 var app = angular.module('delveApp');
 
-app.controller('1890Ctrl', function() {
+app.controller('1890Ctrl', function($scope, $http) {
 	console.log('This page has loaded.');
+
+	$scope.fact = '';
+
+	$scope.getFact = function() {
+		$http({
+		  method: 'GET',
+		  url: 'https://whispering-wave-9659.herokuapp.com/'
+		})
+		.then(function successCallback(response) {
+		    $scope.fact = response.data;
+		  }, function errorCallback(response) {
+		    console.log('error?');
+		});
+	}
 });
