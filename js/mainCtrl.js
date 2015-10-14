bec8e5050baf387070691cd06f68bcf28acfb680
Pam Luangrath <pam.luangrath@gmail.com>
2015-10-14 13:40:27 -0400

    fixed 2015 transition

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -41,8 +41,9 @@ app.controller('mainCtrl', function($scope, factService) {
 		$jq('.year1920').css('background-color', '#fff');
 		$jq('.year1890').css('background-color', '#fff');
 	} else if (cookieYear === '2015') {
-		$jq('#map-content').delay(400).slideDown(500);
-		$jq('#timeline').slideDown(500);
+		$jq('#hide-all').fadeTo(500, 1);
+		$jq('#map-content').slideDown(500);
+		$jq('#timeline').slideDown(200);
 		$jq('#map-content').hide().fadeTo('slow', 1);
 		$jq('#summary-2015').css('display', 'inline-block');
 		$jq('#summary-1920').css('display', 'none');
@@ -53,7 +54,7 @@ app.controller('mainCtrl', function($scope, factService) {
 		$jq('.year1920').css('background-color', '#fff');
 		$jq('.year1890').css('background-color', '#fff');
 	} else {
-		$jq('#hide-all').fadeOut(500);
+		$jq('#hide-all').fadeTo(500, 0);
 		$jq('#timeline').delay(500).slideUp(500);
 		$jq('#map-content').slideUp(500);
 		$jq('.year2015').css('background-color', '#fff');
